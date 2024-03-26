b295ee8e5f53c03b7d05f5a36ace611e38bcf5cf
halt9k <49090368+halt9k@users.noreply.github.com>
2024-03-26 14:20:52 +0300

    wip: basic macro works normally
    (cherry picked from commit 700d0bdd97b670cabae61afcdf2d8e0fdb604510)

--- run-ui-macro.bat
+++ run-ui-macro.bat
@@ -3,7 +3,7 @@ rem This bat is useful to run via shortcut or hotkey
 rem Also consider changing shortcut properties to run minimized  so it did not steal focus
 
 rem Conda env activation
-call activate dev-3-9-18
+call activate dev-3-9-19
 
 rem Alternative run to check import performance if script starts too slowly
 rem python -X importtime src\main.py 2> import.log
--- src/helpers/gui.py
+++ src/helpers/gui.py
@@ -2,27 +2,102 @@ from pathlib import Path
 
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 from mss import mss
 
 # confidence threshold
 # when trying to find, for example, light image on dark theme
 # it's much lower than when light image on light theme with 1.0 perfect match
+from pyrect import Rect
+
 CONFIDENCE_DEFAULT_THRESHOLD = 0.65
 # give warning if above, but not enough to be sure
 CONFIDENCE_WARNING_THRESHOLD = 0.45
 
 
+def debug_show_img(img: np.array):
+	if len(img.shape) == 2:
+		plt.imshow(img, cmap='gray')
+	else:
+		plt.imshow(img)
+	plt.show()
+
+
+# TODO finish later color and scale invariant icon detection
+'''
+def debug_draw(matches, img, img_keypoints, sub_img, sub_keypoints):
+	good_matches = []
+	for m, n in matches:
+		if m.distance < 0.7 * n.distance:
+			good_matches.append(m)
+
+	flags = cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS | cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS
+	matched_img = cv2.drawMatches(sub_img, sub_keypoints, img, img_keypoints, good_matches, None, flags=flags)
+
+	scale = 2
+	scaled_sh = matched_img.shape[1] * scale, matched_img.shape[0] * scale,
+	matched_img_scaled = cv2.resize(matched_img, scaled_sh, interpolation=cv2.INTER_NEAREST)
+	cv2.imshow("Matches", matched_img_scaled)
+
+	sub_keypoints = np.float32([sub_img[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+	img_keypoints = np.float32([img[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+
+	H, mask = cv2.findHomography(sub_keypoints, img_keypoints, cv2.RANSAC, 5.0)
+
+	h, w = sub_img.shape
+	corners = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+	transformed_corners = cv2.perspectiveTransform(corners, H)
+
+	larger_image_with_box = cv2.polylines(img.copy(), [np.int32(transformed_corners)], True, 255, 3, cv2.LINE_AA)
+	cv2.imshow("Sub-image found", larger_image_with_box)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+
+def locate_subimage_canter_surf(img, sub_img, req_confidence):
+	""" Locate even is up/downscaled (but not rotated) """
+
+	# orb = cv2.ORB_create()
+	# patchSize is size of detection area around key point
+	orb = cv2.ORB_create(fastThreshold=6, edgeThreshold=6, patchSize=6)
+
+	img_keypoints, img_desc = orb.detectAndCompute(img, None)
+	sub_keypoints, sub_desc = orb.detectAndCompute(sub_img, None)
+
+	img_desc = img_desc.astype(np.float32)
+	sub_desc = sub_desc.astype(np.float32)
+
+	flann_index_kdtree = 1
+	index_params = dict(algorithm=flann_index_kdtree, trees=5)
+	search_params = dict(checks=5)
+	flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+	matches = flann.knnMatch(sub_desc, img_desc, k=2)
+
+	debug_draw(matches, img, img_keypoints, sub_img, sub_keypoints)
+
+	matches = sorted(matches, key=lambda x: x.distance)
+	sub_img_loc = sub_keypoints[matches[0].trainIdx].pt
+
+	# loc = np.where(matches >= confidence_threshold)
+	min_val, min_confidence, min_loc, pos_lt = cv2.minMaxLoc(matches)
+	pos_center = pos_lt[0] + sub_img.shape[1] / 2, pos_lt[1] + sub_img.shape[0] / 2
+
+	if min_confidence > req_confidence:
+		return pos_center, min_confidence
+	else:
+		return None, min_confidence
+'''
+
+
 def locate_subimage_canter(img, subimage, req_confidence):
+	""" Locate exaclty same image without scaling """
+	
 	matches = cv2.matchTemplate(img, subimage, cv2.TM_CCOEFF_NORMED)
 
 	# loc = np.where(matches >= confidence_threshold)
 	min_val, min_confidence, min_loc, pos_lt = cv2.minMaxLoc(matches)
 	pos_center = pos_lt[0] + subimage.shape[1] / 2, pos_lt[1] + subimage.shape[0] / 2
-
-	# Switch columns and rows
-	# w, h = subimage.shape[:-1]
-	# for pt in zip(*loc[::-1]):
-	#    cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
 
 	if min_confidence > req_confidence:
 		return pos_center, min_confidence
@@ -30,34 +105,54 @@ def locate_subimage_canter(img, subimage, req_confidence):
 		return None, min_confidence
 
 
-def locate_subshape_center(img, subimage, req_confidence):
-	img_edges = cv2.Canny(img, 8, 8)
-	subimage_edges = cv2.Canny(subimage, 8, 8)
+def edge_colors(img: np.array):
+	return np.unique([img[1, 1], img[1, -1], img[-1, -1], img[-1, 1]], axis=0)
+
+
+def replace_colors(image, remove_colors, threshold=4, replace_with=np.array([255, 255, 255])):
+	assert image.ndim == 3 and image.shape[-1] == 3
+	mask = np.zeros_like(False, shape=image.shape[:2], dtype=bool)
+
+	for col in remove_colors:
+		mask = mask | (np.sum(np.abs(image - col), axis=2) < threshold)
+
+	image[mask] = replace_with
+	return image
+
+
+def locate_subshape_center(img, subimg, req_confidence):
+	# img_edges = cv2.Canny(img, 8, 8)
+	# subimage_edges = cv2.Canny(subimage, 8, 8)
+
+	img_no_bck = replace_colors(img, edge_colors(img))
+	subimg_no_bck = replace_colors(subimg, edge_colors(subimg))
 
 	if req_confidence > CONFIDENCE_DEFAULT_THRESHOLD:
 		print(f"Warning: high confidence {req_confidence} may not work for color independent (edge) matching")
 
-	# debug
-	# from matplotlib import pyplot as plt
-	# plt.imshow(img_edges, interpolation='nearest')
-	# plt.show()
+	# return locate_subimage_canter_surf(img_no_bck, subimg_no_bck, req_confidence)
+	return locate_subimage_canter(img_no_bck, subimg_no_bck, req_confidence)
 
-	return locate_subimage_canter(img_edges, subimage_edges, req_confidence)
+
+def same_img_color_type(img1, img2):
+	return len(img1.shape) == len(img2.shape) and img1.shape[-1] == img2.shape[-1]
 
 
 def locate_image_in_screen_region(image_path: Path, expected_region, req_confidence):
 	""" expected_region: if None than search all screen"""
 
 	with mss() as screen:
-		screenshot = np.array(screen.grab(screen.monitors[1]))
-
-	if expected_region:
-		er = expected_region
-		img = screenshot[er.top:er.bottom, er.left:er.right, :]
-	else:
-		img = screenshot
+		monitor = screen.monitors[1]
+		er: Rect = expected_region
+		if er:
+			screenshot = screen.grab((er.left, er.top, er.right, er.bottom))
+		else:
+			screenshot = screen.grab(monitor)
+	img = np.array(screenshot)[:, :, :3]
 
 	subimage = cv2.imread(str(image_path))
+
+	assert same_img_color_type(img, subimage)
 
 	xy, res_confidence = locate_subshape_center(img, subimage, req_confidence)
 	if xy and expected_region:
