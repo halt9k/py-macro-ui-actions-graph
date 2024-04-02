378fb3108815153d2d963d2822041b9a52677c8e
halt9k <49090368+halt9k@users.noreply.github.com>
2024-04-02 16:43:28 +0300

    fix: virtual method decorator improved

--- src/code_tools/virtual_methods.py
+++ src/code_tools/virtual_methods.py
@@ -1,30 +1,32 @@
+import functools
 from functools import wraps
-
 from typing_extensions import override as typing_extensions_override
 
 
 def virutalmethod(method):
     """ Decorator to hint that optional override is possible """
-
     method.__isvirtualmethod__ = True
     return method
 
 
-def check_method(method, self):
-    base_class = self.__class__
-    base_method = getattr(base_class, method.__name__)
+def ensure_ancestor_method_labeled(method, self):
+    for ancestor in self.__class__.__mro__:
+        if ancestor == self.__class__:
+            continue
 
-    is_abstract = getattr(base_method, '__isabstractmeathod__', False)
-    is_virtual = getattr(base_method, '__isvirtualmethod__', False)
-    is_override = getattr(base_method, '__override__', False)
+        base_method = getattr(ancestor, method.__name__, None)
+        if base_method:
+            is_abstract = getattr(base_method, '__isabstractmethod__', False)
+            is_virtual = getattr(base_method, '__isvirtualmethod__', False)
+            # is_override = getattr(base_method, '__override__', False)
+            if is_virtual or is_abstract:
+                return
 
-    if not (is_abstract or is_virtual or is_override):
-        module = base_method.__module__
-        qualname = base_method.__qualname__
-        print(f"WARNING: {module}.{qualname} has no inheritance decorator")
+    module = method.__module__
+    qualname = method.__qualname__
+    print(f"\n WARNING: {module}.{qualname} has no inheritance decorator \n")
 
 
-# TODO implement instead @inheritance_decorators_strict_mode
 def override(method):
     """
     Optional, just verifies inheritance decorators.
@@ -35,7 +37,7 @@ def override(method):
     @typing_extensions_override
     def method_with_override(self, *args, **kwargs):
         if not hasattr(method, "__override_check_done__"):
-            check_method(method, self)
+            ensure_ancestor_method_labeled(method, self)
             method.__override_check_done__ = True
         return method(self, *args, **kwargs)
 
